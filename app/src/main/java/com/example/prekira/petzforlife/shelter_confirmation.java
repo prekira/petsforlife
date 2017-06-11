0fade4bc0d069d95e4fd945d086ee4b65297df69
sarahredwood <sarahxychang@gmail.com>
2017-06-11 05:55:43 -0700

    made all the buttons work kind of

--- app/src/main/java/com/example/prekira/petzforlife/shelter_confirmation.java
+++ app/src/main/java/com/example/prekira/petzforlife/shelter_confirmation.java
@@ -1,7 +1,10 @@
 package com.example.prekira.petzforlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class shelter_confirmation extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class shelter_confirmation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_confirmation);
+
+        final Button soundsgood = (Button) findViewById(R.id.soundsgood);
+        soundsgood.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(shelter_confirmation.this, shelter_main.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/prekira/petzforlife/shelter_hi.java
+++ app/src/main/java/com/example/prekira/petzforlife/shelter_hi.java
@@ -1,7 +1,10 @@
 package com.example.prekira.petzforlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class shelter_hi extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class shelter_hi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_hi);
+
+        final Button next= (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(shelter_hi.this, shelter_allownotifications.class);
+                startActivity(i);
+            }
+        });
     }
 }
